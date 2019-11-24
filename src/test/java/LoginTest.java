f198fcf8be44909107620f0f142b8a9038f91674
epanin <panin.eduard.a@gmail.com>
2019-11-24 17:16:38 +0200

    login and Lead Test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,36 +1,18 @@
-import com.codeborne.selenide.Condition;
+import com.sforce.ws.ConnectionException;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 
 import java.net.URISyntaxException;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class LoginTest {
     @Before
     public void setUp() throws URISyntaxException {
-//        Configuration.startMaximized = false;
-//        Configuration.reportsFolder = "target/surefire-reports";
-
+        TestSalesforceUtil.loginToSalesforce();
     }
-    @Test
-    public void userCanLoginByUsername() throws URISyntaxException {
-        SalesforceLogin login = new SalesforceLogin();
-        String loginUrl = login.loginSample();
-        open(loginUrl);
-        if ($(By.className("switch-to-lightning")).exists()) {
-            $(By.className("switch-to-lightning")).should(Condition.appear).click();
-        }
 
-        String url = url();
-        url = url.replace("setup/SetupOneHome/home", "n/ascendix_ep__Search");
-        open(url);
-//        $(By.name("user.name")).setValue("johny");
-//        $("#submit").click();
-//        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
-//        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
+    @Test
+    public void userCanLoginByUsername() throws URISyntaxException, ConnectionException {
+        TestSalesforceUtil.openTab("Lead");
+        TestSalesforceUtil.clickButton("New");
     }
 }
